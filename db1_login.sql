37301fb0337a2781e9a715ec23cceb3e34747a91
Ozeeleon <ariyaduta06@gmail.com>
2026-04-22 03:29:23 +0700

    Add files via upload

---
+++ db1_login.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, email, password) 
+VALUES 
+('Andi Wijaya', 'andiwijaya@gmail.com', 'pass123'),
+('Siti Aminah', 'sitiaminah@gmail.com', 'pass123'),
+('Rina Pratama', 'rinapratama@gmail.com', 'pass123');
